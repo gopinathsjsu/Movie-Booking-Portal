cdc86da049369e9a220db1f9de01f640b7634ad0
VardhanReddy2 <vardhan.reddy709@gmail.com>
2023-09-29 16:01:45 -0700

    updated .gitignore

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,37 +1,13 @@
-import { Form, message } from "antd";
-import React, { useEffect } from "react";
+import { Form } from "antd";
+import React from "react";
 import Button from "../../components/Button";
-import { Link, useNavigate } from "react-router-dom";
-import { LoginUser } from "../../apicalls/users";
-import { useDispatch } from "react-redux";
-import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate()
-  const onFinish = async (values) => {
-    try {
-      dispatch(ShowLoading())
-      const response = await LoginUser(values);
-      dispatch(HideLoading())
-      if(response.success) {
-        message.success(response.message)
-        localStorage.setItem("token", response.data)
-        window.location.href = '/';
-      } else {
-        dispatch(HideLoading())
-        message.error(response.message)
-      }
-    } catch (error) {
-      message.error(error.message)
-    }
-  }
 
-  useEffect(() => {
-    if(localStorage.getItem("token")) {
-      navigate("/");
-    }
-  }, [])
+  const onFinish = (values) => {
+    console.log("success", values);
+  }
 
   return (
     <div className="flex justify-center h-screen items-center bg-primary">
