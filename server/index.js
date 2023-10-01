71619f79a9fea27e172213c0bbb1847c1bbbbb18
kaushikq-ravindran <kaushikq.ravindran@gmail.com>
2023-10-01 13:13:38 -0700

    small change

--- server/index.js
+++ server/index.js
@@ -20,17 +20,6 @@ app.use("/api/bookings", bookingsRoute);
 
 const port = process.env.PORT || 5000;
 
-const path = require("path");
-__dirname = path.resolve();
-
-// render deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
-
 app.listen(port, ()=> {
     console.log(`Node JS server is running on port ${port}`);
 })
