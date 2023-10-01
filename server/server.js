fd103ddefa86d7e6a46c6328f85f584315ad1891
VardhanReddy2 <vardhan.reddy709@gmail.com>
2023-09-30 23:35:45 -0700

    database connection added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
+
+// Middleware for parsing JSON data
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("MongoDB connected!!"))
+  .catch((err) => console.error("Unable to connect to MongoDB", err));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Node JS - Listening on port ${port}`));
