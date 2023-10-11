35e48b0fb10013f002d646c3e6fb47caabf87430
anudeepk224 <48233893+anudeepk224@users.noreply.github.com>
2023-10-10 20:43:29 -0700

    added basic configuration

--- server/server.js
+++ server/server.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
 const dbConfig = require("./config/dbConfig");
-app.use(express.json());
-
-const usersRoute = require('./routes/usersRoute');
-const moviesRoute = require("./routes/moviesRoute");
-const theatresRoute = require('./routes/theatreRoute');
-
-app.use("/api/users",usersRoute);
-app.use("/api/movies", moviesRoute);
-app.use("/api/theatres", theatresRoute);
 
 const port = process.env.PORT || 5000;
 
