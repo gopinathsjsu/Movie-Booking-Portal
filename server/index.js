661d9328db30159a2cb9d74503b231da56c2ff94
Komal Venugopal Vattumilli <komalvenugopal@gmail.com>
2023-10-20 19:26:11 -0700

    small changes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const dbConfig = require("../config/dbConfig");
+const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 
-const usersRoute = require('../routes/usersRoute');
-const moviesRoute = require("../routes/moviesRoute");
-const theatresRoute = require('../routes/theatreRoute');
-const bookingsRoute = require('../routes/bookingsRoute');
-
-app.get('/deploy-test', (req,res) => {
-    res.send("Deployed successfully")
-})
+const usersRoute = require('./routes/usersRoute');
+const moviesRoute = require("./routes/moviesRoute");
+const theatresRoute = require('./routes/theatreRoute');
+const bookingsRoute = require('./routes/bookingsRoute');
 
 app.use("/api/users",usersRoute);
 app.use("/api/movies", moviesRoute);
