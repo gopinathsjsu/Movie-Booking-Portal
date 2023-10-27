766ba8dc2cf4b232ecc07d6a2f969737ea8fd27c
anudeepk224 <anudeepk224@gmail.com>
2023-10-27 02:50:52 -0700

    added shows

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,58 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Col, Row, Table, message } from "antd";
-import { useDispatch } from "react-redux";
-import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
-import { GetAllMovies } from "../../apicalls/movies";
-import { useNavigate } from 'react-router-dom';
-import moment from "moment";
+import React from 'react'
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [movies, setMovies] = useState([]);
+  return (
+    <div>Home</div>
+  )
+}
 
-  const getData = async () => {
-    try {
-      dispatch(ShowLoading());
-      const response = await GetAllMovies();
-      if (response.success) {
-        setMovies(response.data);
-      } else {
-        message.error(response.message);
-      }
-      dispatch(HideLoading());
-    } catch (error) {
-      dispatch(HideLoading());
-      message.error(error.message);
-    }
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  return <div>
-    <input type="text" className="search-input" placeholder="Search for movie"/>
-    <Row
-      gutter={[20]}
-      className="mt-2"
-    >
-      {movies.map((movie) => (
-        <Col span={6}>
-          <div className="card flex-col gap-1 cursor-pointer" 
-            onClick={()=> navigate(`/movie/${movie._id}?date=${moment().format("YYYY-MM-DD")}`)}
-          >
-            <img src={movie.poster} style={{width:'100%'}} alt="" height={200} />
-            <div className="flex justify-center p-1">
-              <h1 className="text-md uppercase">
-                {movie.title}
-              </h1>
-            </div>
-          </div>
-        </Col>
-      ))}
-    </Row>
-  </div>;
-};
-
-export default Home;
+export default Home
