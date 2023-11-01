a7df0625e95bbb3bf83cb62ef80e55581e6540b4
anudeepk224 <48233893+anudeepk224@users.noreply.github.com>
2023-11-01 14:49:26 -0700

    added backend logic to theaters and cancel bookings

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,18 +1,24 @@
-import React, { useEffect } from "react";
-import { Col, message, Row, Table } from "antd";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { Col, message, Row, Table, Dropdown, Menu } from "antd";
+import { useDispatch, useSelector } from "react-redux";
 import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
 import { GetAllMovies } from "../../apicalls/movies";
 import { useNavigate } from "react-router-dom";
 import moment from "moment";
+import Button from "../../components/Button";
+import { EnvironmentOutlined } from "@ant-design/icons";
 
 function Home() {
   const [searchText = "", setSearchText] = React.useState("");
   const [movies, setMovies] = React.useState([]);
   const [currentlyShowing, setCurrentlyShowing] = React.useState([]);
   const [upcomingMovies, setUpcomingMovies] = React.useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("All Locations");
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.users);
+
+  const locations = ["San Jose", "New York", "Chicago", "All Locations"];
 
   const categorizeMovies = (movies) => {
     const today = moment();
@@ -32,7 +38,12 @@ function Home() {
       dispatch(ShowLoading());
       const response = await GetAllMovies();
       if (response.success) {
-        setMovies(response.data);
+        const filteredMovies = response.data.filter(
+          (movie) =>
+            selectedLocation === "All Locations" ||
+            movie.location === selectedLocation
+        );
+        setMovies(filteredMovies);
         categorizeMovies(response.data);
       } else {
         message.error(response.message);
@@ -46,8 +57,19 @@ function Home() {
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [selectedLocation]);
 
+  const handleLocationChange = ({ key }) => {
+    setSelectedLocation(key);
+  };
+
+  const locationMenu = (
+    <Menu onClick={handleLocationChange}>
+      {locations.map((loc) => (
+        <Menu.Item key={loc}>{loc}</Menu.Item>
+      ))}
+    </Menu>
+  );
   const renderMovies = (movies) => {
     return movies
       .filter((movie) =>
@@ -65,8 +87,12 @@ function Home() {
           >
             <img src={movie.poster} alt="" height={200} />
 
-            <div className="flex justify-center p-1">
+            <div className="flex flex-col justify-center p-1 text-center">
               <h1 className="text-md uppercase">{movie.title}</h1>
+              <h2 className="text-sm">
+                Release Date:
+                {moment(movie.releaseDate).format("MMM Do, YYYY")}
+              </h2>
             </div>
           </div>
         </Col>
@@ -75,21 +101,34 @@ function Home() {
 
   return (
     <div>
-      <input
-        type="text"
-        className="search-input"
-        placeholder="Search for movies"
-        value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-      />
+      {user.membershipType === "Regular" && (
+        <Button
+          title="Subscribe to Premium"
+          onClick={() => navigate("/premium")}
+        />
+      )}
 
-      <h2 className="text-lg mt-4 mb-2">Currently Showing</h2>
-      <Row gutter={[20]}>{renderMovies(currentlyShowing)}</Row>
+      <div style={{ marginBottom: "16px" }}>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search for movies"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+      <Dropdown overlay={locationMenu} trigger={["click"]}>
+        <a
+          className="ant-dropdown-link"
+          onClick={(e) => e.preventDefault()}
+          style={{ cursor: "pointer" }}
+        >
+          <EnvironmentOutlined style={{ marginRight: 8 }} /> {/* Icon */}
+          {selectedLocation} {/* Title */}
+        </a>
+      </Dropdown>
 
-      <h2 className="text-lg mt-4 mb-2">Upcoming Movies</h2>
-      <Row gutter={[20]}>{renderMovies(upcomingMovies)}</Row>
-
-      {/* <Row gutter={[20]} className="mt-2">
+      <Row gutter={[20]} className="mt-2">
         {movies
           .filter((movie) =>
             movie.title.toLowerCase().includes(searchText.toLowerCase())
@@ -112,7 +151,34 @@ function Home() {
               </div>
             </Col>
           ))}
-      </Row> */}
+      </Row>
+      <br />
+      <br />
+
+      <h2 className="text-lg mt-4 mb-2">Currently Showing</h2>
+      <Row gutter={[20]}>
+        {renderMovies(
+          currentlyShowing.filter(
+            (movie) =>
+              selectedLocation === "All Locations" ||
+              movie.location === selectedLocation
+          )
+        )}
+      </Row>
+      <br />
+      <br />
+
+      <h2 className="text-lg mt-4 mb-2">Upcoming Movies</h2>
+      <Row gutter={[20]}>
+        {renderMovies(
+          upcomingMovies.filter(
+            (movie) =>
+              selectedLocation === "All Locations" ||
+              movie.location === selectedLocation
+          )
+        )}
+      </Row>
+      <br />
     </div>
   );
 }
