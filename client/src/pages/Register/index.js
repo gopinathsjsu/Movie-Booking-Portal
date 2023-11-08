dd8be668917baeb25580c7600f4b14818c579466
Komal Venugopal Vattumilli <komalvenugopal@gmail.com>
2023-11-08 07:41:44 -0800

    added register screen in frontend

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,36 +1,15 @@
-import { Form, message } from "antd";
-import React, { useEffect } from "react";
+import { Form } from "antd";
+import React from "react";
 import Button from "../../components/Button";
-import { Link, useNavigate } from "react-router-dom";
-import { RegisterUser } from "../../apicalls/users";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  const navigate = useNavigate();
-  const onFinish = async (values) => {
-    try {
-      const response = await RegisterUser(values);
-      if(response.success) {
-        message.success(response.message)
-      } else {
-        message.error(response.message)
-      }
-    } catch (error) {
-      message.error(error.message)
-    }
-  }
-
-    useEffect(() => {
-      if (localStorage.getItem("token")) {
-        navigate("/");
-      }
-    }, [])
-
   return (
     <div className="flex justify-center h-screen items-center bg-primary">
       <div className="card p-3 w-400">
-        <h1 className="text-xl mb-1">BOOKMYMOVIE - REGISTER</h1>
+        <h1 className="text-xl mb-2">BOOKMYMOVIE - REGISTER</h1>
         <hr />
-        <Form layout="vertical" className="mt-1" onFinish={onFinish}>
+        <Form layout="vertical" className="mt-2">
           <Form.Item
             label="Name"
             name="name"
