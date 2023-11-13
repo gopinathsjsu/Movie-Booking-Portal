97ebb4472fb210a736d2cc8bdd0ce3e900d7ee85
Komal Venugopal Vattumilli <komalvenugopal@gmail.com>
2023-11-12 20:01:39 -0800

    worked on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -11,61 +11,21 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import { useSelector } from "react-redux";
 import Profile from "./pages/Profile";
 import Admin from "./pages/Admin";
-import TheatresForMovie from "./pages/TheatresForMovie";
-import BookShow from "./pages/BookShow";
 
 function App() {
+
   const { loading } = useSelector((state) => state.loaders);
 
   return (
     <div>
-      {loading && (
-        <div className="loader-parent">
+      {loading && <div className="loader-parent">
           <div className="loader"></div>
-        </div>
-      )}
+        </div>}
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/movie/:id"
-            element={
-              <ProtectedRoute>
-                <TheatresForMovie />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/book-show/:id"
-            element={
-              <ProtectedRoute>
-                <BookShow />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute>
-                <Admin />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route path="/admin" element={<ProtectedRoute><Admin/></ProtectedRoute>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
